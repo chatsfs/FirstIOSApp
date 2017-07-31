da1cc09d038bff5d746880df4e6e89b7d7e6f4f0
Quokka <raul.bigoria@holinsys.pe>
2017-07-31 00:05:06 -0500

    First commit

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var funFactLabel: UILabel!
+    let facts = ["Ants stretch when they wake up in the morning", "Ostriches can run faster than horses"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        funFactLabel.text = facts[0]
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func showFact() {
 
+        
+        funFactLabel.text=facts[]
+    }
 
 }
 
